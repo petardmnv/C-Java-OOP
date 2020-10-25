5b3d2deccc1ee08365ca2aa35872cbe3f66ceb7c
petardmnv <petar.dmnv@gmail.com>
2020-10-25 04:23:16 -0700

    Now Guitar.cpp is working!

--- Classwork/Guitar.cpp
+++ Classwork/Guitar.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
@@ -11,52 +12,52 @@ double wight;
 };
 class Giutar{
 public:
-	Strings string_array[6] = {0,0,0,0,0,0};
+	Strings string_array[6] = {0, 0, 0, 0, 0, 0};
     void add_string(int index, Strings my_string){
-        if (this->my_string.wight > 6 || my_string.wight < 0.2){
+        if (my_string.wight > 6 || my_string.wight < 0.2){
             throw "string wight is not awolled";
             
         }
-        for (int i = 0; i < 6; i++) {
+        for (int i = 1; i < 5; i++) {
+			/*if (string_array[i] ==0){
+				continue;
+			}
+			if (string_array[i] > string_array[i - 1]){
+				throw "Strings are not increasing";
+			}
+			if (string_array[i] < string_array[i + 1]){
+				throw "Strings are not increasing";
+			}*/
             if (i < index){
-                if (string_array[i] > string_array[index]){
+                if (string_array[i].wight > my_string.wight){
                     throw "Strings are not increasing";
                 }
             }else if(index < i){
-                if (string_array[i] == 0){
+                if (string_array[i].wight == 0){
                     continue;
                 }
-                if (string_array[i] < string_array[index]){
+                if (string_array[i].wight < my_string.wight){
                     throw "Strings are not increasing";
                 }
             }
         }
+        this->string_array[index] = my_string;
     }
 };
 
 int main()
 {
-    class Strings my_string;
-    my_string.Strings(1);
-    class Giutar my_guitar;
-    try{
-        my_guitar.add_string(1, my_string);
-    }catch(const char * ex){
-        cout<<ex<<endl;
+    Giutar my_guitar;
+    for (int i = 0; i < 6; ++i){
+    	Strings s(i + pow(-1.23, i));
+    	try{
+       		my_guitar.add_string(i, s);
+    	}catch(const char * ex){
+       		cerr<<ex<<endl;
+    	}
     }
-    my_string.Strings(0.5);
-    try{
-        my_guitar.add_string(0, my_string);
-    }catch(const char * ex){
-        cout<<ex<<endl;
+    for (int i = 0; i < 6; ++i){
+    	cout << my_guitar.string_array[i].wight << endl;
     }
-    my_string.Strings(3);
-        try{
-        my_guitar.add_string(, my_string);
-    }catch(const char * ex){
-        cout<<ex<<endl;
-    }
-    /*my_string.Strings(4);*/
-    
     return 0;
 }
