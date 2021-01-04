df55a54d64d0f336a6a180bcd6aaa70a450cb5aa
petardmnv <petar.dmnv@gmail.com>
2021-01-04 16:40:25 +0200

    Add some  classwork

---
+++ Classwork/T 04.01Practice/online_banking.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Bills{
+	double credit_limit;
+public:
+	BILLs();
+};
+
+class Account{
+	string IBAN;
+	double money; 
+	double get_money(){
+		return this->money;
+	}
+	void set_money(double new_money){
+		this->money = new_money;
+	}
+	string get_IBAN(){
+		return this->IBAN;
+	}
+	void set_IBAN(string new_iban){
+		this->IBAN = new_iban;
+	}
+	Account();
+	Account(string iban, double money){
+		this->IBAN = iban;
+		this->money = money;
+	}
+
+	void pay(double bill){
+		if (this->money < bill){
+			throw "Not enough money!";
+		}
+		this->money -= bill;
+
+	}
+	void borrow(){
+
+	}
+};
+
+class User{
+	string username;
+	double password;
+
+public:
+	User();
+};
+
+int main(int argc, char const *argv[])
+{
+	/* code */
+	return 0;
+}
+
+		
