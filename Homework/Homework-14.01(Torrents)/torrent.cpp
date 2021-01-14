6f888dd169a288b80f3db8adda7fab1231e098d0
petardmnv <petar.dmnv@gmail.com>
2021-01-15 00:18:39 +0200

    Add some work

---
+++ Homework/Homework-14.01(Torrents)/torrent.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Torrent{
+	string title;
+	int size;
+	string uploader;
+	int downloads_count;
+public:
+	Torrent(string title, int size, string uploader,
+		int downloads_count){
+		this->title = title;
+		this->size = size;
+		this->uploader = uploader;
+		this->downloads_count = downloads_count;
+	}
+	string getTitle() const { return this->title; }
+	int getSize() const { return this->size; }
+	string getUploader() const { return this->uploader; }
+	int getDownloadsCount() const { return this->downloads_count; }
+
+};
+
+
+class GameTorrent : public Torrent{
+	string system;
+	char maturity_rating;
+
+public:
+	GameTorrent(string title, int size, string uploader,
+		int downloads_count, char maturity_rating) : Torrent(title, size, uploader, downloads_count){
+		if (maturity_rating != 'E' && maturity_rating != 'M', maturity_rating != 'P'){
+			throw "Invallid maturity_rating value";
+		}
+		this->maturity_rating = maturity_rating;
+	}
+	char getMaturityRating() const { return this->maturity_rating; }
+};
+
+
+class FilmTorrent : public Torrent{
+	string director;
+	int duration;
+	string language;
+public:
+	FilmTorrent(string title, int size, string uploader,
+		int downloads_count, string director, int duration, string language) : Torrent(title, size, uploader, downloads_count){
+		this->director = director;
+		this->duration = duration;
+		this->language = language;
+	}
+	string getDirector() const { return this->director; }
+	int getDuration() const { return this->duration; }
+	string getLanguage() const { return this->language; }
+};
+
+
+class SoftwareTorrent : public Torrent{
+	string producer;
+	string system;
+	// Tri 4isla sth
+public:
+
+} ;
+
+int main(int argc, char const *argv[]){
+
+	return 0;
+}
