6c479f6cb41efae415b226695e66e581fbbbad18
petardmnv <petar.dmnv@gmail.com>
2021-04-06 22:37:11 +0300

    Add some work

---
+++ Exams/Exam-Java(18.03)/src/com/company/MonsterTest.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import static org.junit.jupiter.api.Assertions.*;
+class MonsterTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getPower() {
+    }
+}
---
+++ Exams/Exam-Java(18.03)/src/com/company/WarriorTest.java
@@ -0,0 +1,11 @@
+package com.company;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class WarriorTest {
+
+    @Test
+    void getPower() {
+    }
+}
