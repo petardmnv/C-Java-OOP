7968731588e9ca91ef6976f1976d7cb55a92ec66
petardmnv <petar.dmnv@gmail.com>
2021-06-03 11:26:12 +0300

    Add start up

--- Homework/ParkingSpaces/src/ParkingPlace.java
+++ Homework/ParkingSpaces/src/ParkingPlace.java
@@ -1,2 +1,26 @@
 public class ParkingPlace {
+    private int number;
+    private double area;
+    private String personName;
+    private String personSurname;
+    private String description;
+
+    public ParkingPlace(int number, double area, String personName, String personSurname, String description) {
+        this.number = number;
+        this.area = area;
+        this.personName = personName;
+        this.personSurname = personSurname;
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "ParkingPlace{" +
+                "number=" + number +
+                ", area=" + area +
+                ", personName='" + personName + '\'' +
+                ", personSurname='" + personSurname + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+    }
 }
