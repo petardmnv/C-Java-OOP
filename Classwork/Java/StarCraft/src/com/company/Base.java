588d15409cfbd2ac0858072dfe4accb8e845f3a6
petardmnv <petar.dmnv@gmail.com>
2021-06-26 16:54:26 +0300

    Add lastly changes

--- Classwork/Java/StarCraft/src/com/company/Base.java
+++ Classwork/Java/StarCraft/src/com/company/Base.java
@@ -1,6 +1,81 @@
 package com.company;
 
-public class Base {
-    // Create workers and collect resourses
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Base{
+    // Create workers and collect resources
+    private int resource;
+    List<Worker> workers = new ArrayList<>();
+    Map<Crystal, Integer> usedMines = new HashMap<>();
+    private List<Crystal> crystals = new ArrayList<>();
+    public Base(){
+
+    }
+
+    public Base(List<Worker> workers) {
+        this.resource = 0;
+        this.workers = workers;
+    }
+
+    public List<Crystal> getCrystals() {
+        return crystals;
+    }
+
+    private void setUsedMines(){
+        for (Crystal c: crystals){
+            usedMines.put(c, 0);
+        }
+    }
+
+    public synchronized boolean addWorkerToMine(Crystal c){
+        int using = usedMines.get(c);
+        if(using < 2){
+            usedMines.put(c, using + 1);
+            return true;
+        }
+        return false;
+    }
+
+    public synchronized Map<Crystal, Integer> getUsedMines() {
+        return usedMines;
+    }
+
+    public synchronized void removeMine(Crystal c){
+        if(crystals.contains(c)) {
+            crystals.remove(c);
+        }
+    }
+
+    public synchronized Crystal getMine(){
+        if(crystals.size() == 0){
+            return null;
+        }
+        return crystals.get(0);
+    }
+    public synchronized  int getMinesCount(){
+        return crystals.size();
+    }
+
+    public synchronized void removeWorkerToMine(Crystal c){
+        int using = usedMines.get(c);
+        if(using >= 1){
+            usedMines.put(c, using - 1);
+        }
+    }
+
+    public synchronized void addToResource(int amount){
+        this.resource += amount;
+    }
+
+    public void addWorker(Worker worker){
+        workers.add(worker);
+    }
+
+    public void startMining(){
+        
+    }
 
 }
--- Classwork/Java/StarCraft/src/com/company/Worker.java
+++ Classwork/Java/StarCraft/src/com/company/Worker.java
@@ -1,33 +1,114 @@
 package com.company;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.Semaphore;
+
+import static java.lang.Thread.sleep;
 
 public class Worker implements Runnable {
     // Work - collect crystals from crystal mine
     // Workers work alone. They are threads
-    private List<Crystal> crystals = new ArrayList<>();
+    Map<Crystal, Integer> usedMines = new HashMap<>();
     private int id;
-    private int mined_crystal;
+    private int minedCrystal;
+    private Base base;
 
-    public Worker(List<Crystal> crystals, int id, int mined_crystal) {
-        this.crystals = crystals;
+    public Worker(Base base, int id, int minedCrystal) {
+        this.base = base;
         this.id = id;
-        this.mined_crystal = mined_crystal;
+        this.minedCrystal = minedCrystal;
     }
 
+    public synchronized int getMinedCrystal() {
+        return minedCrystal;
+    }
 
+    public synchronized void addToMinedCrystal(int minedCrystal) {
+        this.minedCrystal += minedCrystal;
+    }
 
-    public int getId() {
+    public synchronized int getId() {
         return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
     }
 
     @Override
     public void run() {
+        // See if mine is empty
+        // Mine if not or switch mine
+        // if no mines available stop
+        while (base.getMinesCount() > 0){
+            Crystal mine = goingToMine();
+            try{
+                while(getMinedCrystal() < 20){
+                    if(mine == null){
+                        break;
+                    }
+                    System.out.printf("Miner %d is mining from block %d", id, mine.getMineId());
+                    sleep(1500);
+                    int neededCrystals = getMinedCrystal();
+                    int amount = mine.subtractFromCrystalCount(20 - neededCrystals);
+                    addToMinedCrystal(amount);
+                    if(amount == 0){
+                        base.removeMine(mine);
+                        base.removeWorkerToMine(mine);
+                        mine = goingToMine();
+                    }
+                }
+            }
+            catch (InterruptedException e)
+            {
+                e.printStackTrace();
+            }
+            base.removeWorkerToMine(mine);
+            try {
+                sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            base.addToResource(minedCrystal);
 
+            try {
+                sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+        }
     }
+    public synchronized Crystal goingToMine() {
+        int time = 1000;
+        Crystal mine = base.getMine();
+        if (mine == null) {
+            return null;
+        }
+        try {
+            System.out.printf("Worker with id %d is going to mine %d", id, mine.getMineId());
+            sleep(time);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        if (!base.addWorkerToMine(mine)) {
+            while (true) {
+                System.out.printf("Too many miners in the mine. Worker %d going to next mine...", id);
+                time = 500;
+                try {
+                    sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                mine = base.getMine();
+                if (mine == null) {
+                    return null;
+                }
+                if (base.addWorkerToMine(mine)) {
+                    break;
+                }
+            }
+        }
+        return mine;
+    }
+
 }
